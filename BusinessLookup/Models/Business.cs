c990c2e3c93ce410819b1c135142e746b44189af
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-04-03 14:35:08 -0700

    add business class and properties.

---
+++ BusinessLookup/Models/Business.cs
@@ -0,0 +1,11 @@
+namespace CretaceousPark.Models
+{
+    public class Business
+    {
+        public int BusinessId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Established { get; set; }
+        public string Owner { get; set; }
+    }
+}
