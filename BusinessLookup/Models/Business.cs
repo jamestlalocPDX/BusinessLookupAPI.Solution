12955b362508c4e02284e4e4a8bc244c881bc775
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-04-13 01:14:29 -0700

    add validations to Business properties.

--- BusinessLookup/Models/Business.cs
+++ BusinessLookup/Models/Business.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BusinessLookup.Models
 {
     public class Business
     {
         public int BusinessId { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(200)]
         public string Description { get; set; }
-        public int Established { get; set; }
+        [Required]
+        [StringLength(4)]
+        public string Established { get; set; }
+        [Required]
+        [StringLength(50)]
         public string Owner { get; set; }
     }
 }
