17dead754a2ff1245f312df3e7b7132cfa5549cd
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-04-13 02:24:17 -0700

    seed data.

---
+++ BusinessLookup/Migrations/20200413090254_SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BusinessLookup.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Businesses",
+                columns: new[] { "BusinessId", "Description", "Established", "Name", "Owner" },
+                values: new object[,]
+                {
+                    { 1, "We sell tom foolery items.", "2004", "Hooligans", "John" },
+                    { 2, "We sell golfing equipment and more.", "1993", "Golf N Stuff", "Ernesto" },
+                    { 3, "We sell the latest tech cell phones.", "2007", "Phones and More", "George" },
+                    { 4, "We sell the gnarliest surfboards.", "1978", "Surfboards R Us", "Andy" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Businesses",
+                keyColumn: "BusinessId",
+                keyValue: 4);
+        }
+    }
+}
--- BusinessLookup/Models/BusinessLookupContext.cs
+++ BusinessLookup/Models/BusinessLookupContext.cs
@@ -2,13 +2,24 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BusinessLookup.Models
 {
-    public class BusinessLookupContext : DbContext
+  public class BusinessLookupContext : DbContext
+  {
+    public BusinessLookupContext(DbContextOptions<BusinessLookupContext> options)
+        : base(options)
     {
-        public BusinessLookupContext(DbContextOptions<BusinessLookupContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Business> Businesses { get; set; }
     }
+
+    public DbSet<Business> Businesses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Business>()
+      .HasData(
+        new Business { BusinessId = 1, Name = "Hooligans", Description = "We sell tom foolery items.", Established = "2004", Owner = "John" },
+        new Business { BusinessId = 2, Name = "Golf N Stuff", Description = "We sell golfing equipment and more.", Established = "1993", Owner = "Ernesto" },
+        new Business { BusinessId = 3, Name = "Phones and More", Description = "We sell the latest tech cell phones.", Established = "2007", Owner = "George" },
+        new Business { BusinessId = 4, Name = "Surfboards R Us", Description = "We sell the gnarliest surfboards.", Established = "1978", Owner = "Andy" }
+    );
+    }
+  }
 }
