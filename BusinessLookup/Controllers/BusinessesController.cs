bad0001c51ae78a103020835f41d759af042abbf
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-04-13 00:12:06 -0700

    add Businesses Controller file.

---
+++ BusinessLookup/Controllers/BusinessesController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using BusinessLookup.Models;
+
+namespace BusinessLookup.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BusinessesController : ControllerBase
+  {
+    private BusinessLookupContext _db;
+
+    public BusinessesController(BusinessLookupContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/businesses
+    [HttpGet]
+    public ActionResult<IEnumerable<Business>> Get()
+    {
+      return _db.Businesses.ToList();
+    }
+
+    // POST api/businesses
+    [HttpPost]
+    public void Post([FromBody] Business business)
+    {
+      _db.Businesses.Add(business);
+      _db.SaveChanges();
+    }
+  }
+}
